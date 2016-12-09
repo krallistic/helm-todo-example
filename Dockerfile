d9eca03487a62ddf78959cb50b887c71cde64f44
Jakob Karalus <jakobkaralus@Jakobs-MacBook-Pro.local>
2016-12-09 13:28:52 +0100

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:2.7
+MAINTAINER Jakob Karalus jakob.karalus@codecentric.de
+ADD . /todo
+WORKDIR /todo
+RUN pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient(
+    os.environ['MONGO_DB_ADDRESS'],
+    os.environ['MONGO_DB_PORT'])
+db = client.tododb
+
+
+@app.route('/')
+def todo():
+    todos = [todo for todo in db.tododb.find()]
+
+    return render_template('todo.html', items=todos)
+
+
+@app.route('/new', methods=['POST'])
+def new():
+
+    todo_doc = {
+        'title': request.form['title'],
+        'description': request.form['description'],
+        'date': request.form['date']
+    }
+    db.tododb.insert_one(todo_doc)
+
+    return redirect(url_for('todo'))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
